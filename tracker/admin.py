c69d73de786907f128fea9b94034feaeef0724d8
Maria Lukowich <mlukowich27@gmail.com>
2024-05-01 17:50:47 -0600

    Admin Interface

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User,Workout, Goal, WaterIntake
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Workout)
+admin.site.register(Goal)
+admin.site.register(WaterIntake)
