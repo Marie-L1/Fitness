9dc3ec4ae63bf3cef5964b92cf9279f19fdae31a
Maria Lukowich <mlukowich27@gmail.com>
2024-05-16 12:38:36 -0600

    updating urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("new_goal/", views.new_goal, name="new_goal"),
     path("profile/", views.user_profile, name="user_profile"),
-    path("edit_workout/<int:workout_id>", views.edit_workout, name="edit_workout")
+    path("edit_workout/<int:workout_id>", views.edit_workout, name="edit_workout"),
+    path("mental_health/", views.mental_heatlh, name="mental_health")
 ]
