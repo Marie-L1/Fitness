b6302056106ccbd0ca569a4fc6eb5a7b0d6bd193
Maria Lukowich <mlukowich27@gmail.com>
2024-05-27 14:14:41 -0600

    editing url patterns

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("register/", views.register, name="register"),
     path("new_goal/", views.new_goal, name="new_goal"),
     path("profile/", views.user_profile, name="user_profile"),
     path("edit_workout/<int:workout_id>", views.edit_workout, name="edit_workout"),
