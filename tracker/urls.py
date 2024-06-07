9b0617392260c1bfc2e6e467d600286d56194985
Maria Lukowich <mlukowich27@gmail.com>
2024-06-07 14:49:26 -0600

    updating index goal checklist

--- tracker/urls.py
+++ tracker/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
     path("new_goal/", views.new_goal, name="new_goal"),
+    path("edit_goal/", views.edit_goal, name="edit_goal"),
     path("profile/", views.user_profile, name="user_profile"),
     path("edit_workout/<int:workout_id>", views.edit_workout, name="edit_workout"),
     path("mental_health/", views.mental_health, name="mental_health"),
