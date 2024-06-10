431df8948de4f15a1395d9e86dad56290e5a6654
Maria Lukowich <mlukowich27@gmail.com>
2024-06-10 14:21:29 -0600

    updating libraries

--- tracker/views.py
+++ tracker/views.py
@@ -17,6 +17,7 @@ import calendar
 from collections import defaultdict
 from datetime import datetime
 import numpy as np    # for array manipulation
+import json
 
 logger = logging.getLogger(__name__)
 
