37915ce06d6d1a2e3ed5812ce5b0b7fe5fd884be
Maria Lukowich <mlukowich27@gmail.com>
2024-06-17 15:17:59 -0600

    updating url patterns

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,6 +5,7 @@ app_name = 'tracker'
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("homepage/", views.homepage, name="homepage"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("register/", views.register, name="register"),
