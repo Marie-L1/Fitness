adca45a38c48977d99e256b8a85f77594b88b4e8
Maria Lukowich <mlukowich27@gmail.com>
2024-06-18 11:17:16 -0600

    adding models to admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,Workout, Goal, WaterIntake
+from .models import User,Workout, Goal, WaterIntake, MentalHealth
 
 
 # Register your models here.
@@ -7,3 +7,4 @@ admin.site.register(User)
 admin.site.register(Workout)
 admin.site.register(Goal)
 admin.site.register(WaterIntake)
+admin.site.register(MentalHealth)
