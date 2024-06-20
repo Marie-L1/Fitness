49ae70d70e842fe70c68bde66fc343ab0751dc86
Maria Lukowich <mlukowich27@gmail.com>
2024-06-20 12:23:46 -0600

    debugging mental health summary

--- tracker/views.py
+++ tracker/views.py
@@ -274,11 +274,21 @@ def mental_health_summary(request):
         # Filter the entries for the current user and the current month
         mental_health_entries = MentalHealth.objects.filter(user=request.user, date__month=current_month)
 
+        # debugging 
+        print("Mental Health Entries:", mental_health_entries)
+
         emotions = mental_health_entries.values("emotion").annotate(count=Count("emotion"))
         daily_gratitude = mental_health_entries.values("date", "daily_gratitude")
         self_care_habits = mental_health_entries.values("date", "self_care_habit")
         energy_levels = mental_health_entries.values("date", "energy_level")
         rants = mental_health_entries.values("date", "rant")
+
+        # debugging
+        print("Emotions:", emotions)
+        print("Daily Gratitude:", daily_gratitude)
+        print("Self Care Habits:", self_care_habits)
+        print("Energy Levels:", energy_levels)
+        print("Rants:", rants)
 
         # chart generations
         emotion_chart = generate_emotion_chart(request.user)
